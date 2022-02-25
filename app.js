64b4562317457c5c15d1009b96a8607222ec4aeb
Leo <a131234568@gmail.com>
2022-02-25 17:28:08 +0800

    我的版本

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
 const exphbs = require('express-handlebars')
+const movieList = require('./movies.json')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -10,49 +10,30 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  const movieList = [
-    {
-      id: 1,
-      title: 'Jurassic World: Fallen Kingdom',
-      image: 'https://movie-list.alphacamp.io/posters/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg'
-    },
-    {
-      id: 2,
-      title: 'THIS IS MOVIE TITLE',
-      image: 'https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg'
-    }, {
-      id: 3,
-      title: "Thor: Ragnarok",
-      image: "https://movie-list.alphacamp.io/posters/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg"
-    },
-    {
-      id: 4,
-      title: "Avengers: Infinity War",
-      image: "https://movie-list.alphacamp.io/posters/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg"
-    },
-    {
-      id: 5,
-      title: "Mission: Impossible - Fallout",
-      image: "https://movie-list.alphacamp.io/posters/80PWnSTkygi3QWWmJ3hrAwqvLnO.jpg"
-    },
-    {
-      id: 6,
-      title: "Incredibles 2",
-      image: "https://movie-list.alphacamp.io/posters/x1txcDXkcM65gl7w20PwYSxAYah.jpg"
-    },
-    {
-      id: 7,
-      title: "Fifty Shades Freed",
-      image: "https://movie-list.alphacamp.io/posters/jjPJ4s3DWZZvI4vw8Xfi4Vqa1Q8.jpg"
-    },
-    {
-      id: 8,
-      title: "The First Purge",
-      image: "https://movie-list.alphacamp.io/posters/2slvblTroiT1lY9bYLK7Amigo1k.jpg"
-    },
-  ]
 
-  res.render('index', { movies: movieList })
+  res.render('index', { movies: movieList.results })
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  const movieOne = {
+    id: 0,
+    title: '',
+    description: '',
+    release_date: '',
+    image: ''
+  }
+
+  movieList.results.forEach((item) => {
+    if (item.id === Number(req.params.movie_id)) {
+      movieOne.id = item.id
+      movieOne.title = item.title
+      movieOne.description = item.description
+      movieOne.release_date = item.release_date
+      movieOne.image = item.image
+    }
+  } )
+
+  res.render('show', { movie: movieOne })
 })
 
 app.listen(port, () => {
